0397fdcf1b8ad87268a44bcf6dca7bbd48a3e8bc
elenamorton <elena.uleia@gmail.com>
2017-06-07 13:22:49 +0100

    Adding stormy conditions to airport spec. 10TC passing, 3TC failing

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -23,12 +23,23 @@ describe('Feature Test:', function (){
     plane.land(airport);
     plane.takeoff();
     expect(airport.planes()).not.toContain(plane);
+   });
   });
     
-  it('blocks tackoff when the weather is stormy', function () {
+ describe('under stormy conditions',function(){
+     
+  it('blocks takeoff when the weather is stormy', function () {
+      spyOn(Math,'random').and.returnValue(0);
       plane.land(airport);
       spyOn(airport, 'isStormy').and.returnValue(true);
       expect(function() { plane.takeoff(); }).toThrowError('cannot takeoff during storm');
       expect(airport.planes()).toContain(plane);
   });
+    
+  it('blocks landing when weather is stormy', function(){
+      spyOn(Math,'random').and.returnValue(1);
+      expect(function(){ plane.land(airport); }).toThrowError('cannot land during storm');
+      expect(airport.planes()).toEqual([]);
+    });
+ });
 });
